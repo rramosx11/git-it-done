974f21a18d28ea91e7a9e44fd173a10d67ad3b00
rramosx11 <rodolforamosd11@gmail.com>
2022-07-10 00:15:01 -0400

    language was added

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,9 +3,19 @@ var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoName = function () {
+  // grab repo name from url query string
   var queryString = document.location.search;
   var repoName = queryString.split("=")[1];
-  console.log(repoName);
+
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
 };
 
 var getRepoIssues = function (repo) {
@@ -85,4 +95,3 @@ var displayWarning = function (repo) {
 };
 
 getRepoName();
-getRepoIssues();
